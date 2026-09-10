358410d295f4981ce191f524cb175c692dd6342a
saumitraid <saumitraid.das@gmail.com>
2026-09-10 22:14:58 +0530

    change urls

--- testproject/urls.py
+++ testproject/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('app.urls')),
-    path('admin', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
